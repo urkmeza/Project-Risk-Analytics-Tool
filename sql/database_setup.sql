bb147d5e1ee0d14ab751395f0eb6bc5774635927
urkmeza <125884362+urkmeza@users.noreply.github.com>
2026-01-13 16:59:05 +0300

    Fix comment formatting and typo in database_setup.sql
    Corrected comment formatting and typo in data entry sample.

--- sql/database_setup.sql
+++ sql/database_setup.sql
@@ -6,9 +6,9 @@ CREATE TABLE ProjectTasks (
     OptimisticDays INT, ## Best case
     PessimisticDays INT, ## Worst case
     RiskFactor FLOAT, ##  0.1 to 1.0 (Side effects, supplier risks etc.)
-    [cite_start]StakeholderComplexity INT ## 1 to 5 
+    StakeholderComplexity INT ## 1 to 5 
 );
 
-## Data entery sample (EG: Factory establishment in EG
+## Data entery sample (e.g.: Factory establishment in EG)
 INSERT INTO ProjectTasks VALUES (1, 'Production Line Transfer', 45, 40, 65, 0.4, 5);
 INSERT INTO ProjectTasks VALUES (2, 'Tool Installation', 30, 25, 45, 0.2, 3);
